d05bc375e0d7d367edeb324e18f54e6427909456
Snowman64 <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-08-17 14:03:59 -0400

    Intro Sequence can be Skipped

--- Escapeaway/Source/States/IntroState.cs
+++ Escapeaway/Source/States/IntroState.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Escapeaway.Source.Objects.Intro;
 
 namespace Escapeaway.Source.States
@@ -22,6 +23,14 @@ namespace Escapeaway.Source.States
         {
             snowman64.Update(gameTime);
 
+            // Skip Sequence
+            if (KeyPress(Keys.Z) || KeyPress(Keys.X) || KeyPress(Keys.Escape) || KeyPress(Keys.Enter)
+                || ButtonPress(Buttons.A) || ButtonPress(Buttons.B) || ButtonPress(Buttons.Start))
+            {
+                SwitchState(main.title);
+            }
+
+            // Go to Title When Sequence Over
             if (snowman64.gone) SwitchState(main.title);
         }
 
